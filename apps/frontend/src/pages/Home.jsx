5c1547d986f6d66c4f560ad13dff519f843c56d5
FelpSousaS <felipess.dev@gmail.com>
2024-07-17 15:00:59 -0300

    add Home

---
+++ apps/frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from '../components/Hero';
+import Layout from '../components/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <div>
+        <Hero />
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
