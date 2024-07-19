f51a0600fb4187f952226bd7640024b8543616a4
FelpSousaS <felipess.dev@gmail.com>
2024-07-19 14:03:29 -0300

    ajustes no Layout

--- apps/frontend/src/components/Layout.jsx
+++ apps/frontend/src/components/Layout.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from './NavBar';
 import Footer from './Footer';
+import { Box } from '@chakra-ui/react';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Box display="flex" flexDirection="column" minHeight="100vh">
       <Navbar />
-      <main>{children}</main>
+      <Box as="main" flex="1">
+        {children}
+      </Box>
       <Footer />
-    </>
+    </Box>
   );
 };
 
