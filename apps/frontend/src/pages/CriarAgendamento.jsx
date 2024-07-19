1899d7020846e4d891269835904520af6fd6ed6f
FelpSousaS <felipess.dev@gmail.com>
2024-07-19 01:21:52 -0300

    criando pagina de agendamento

--- apps/frontend/src/pages/CriarAgendamento.jsx
+++ apps/frontend/src/pages/CriarAgendamento.jsx
@@ -1,3 +1,12 @@
-const CriarAgendamento = () => <div></div>;
+import Layout from '../components/Layout';
+import FormAgendamento from '../components/FormAgendamento';
+import PageTitle from '../components/PageTitle';
+
+const CriarAgendamento = () => (
+  <Layout>
+    <PageTitle>Agendar</PageTitle>
+    <FormAgendamento />
+  </Layout>
+);
 
 export default CriarAgendamento;
