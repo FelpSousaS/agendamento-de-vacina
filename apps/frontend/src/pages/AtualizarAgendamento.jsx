01eae371ba2014e6b3f1af8730acadfc533441f1
FelpSousaS <felipess.dev@gmail.com>
2024-07-19 01:27:38 -0300

    refatorando rotas

--- apps/frontend/src/pages/AtualizarAgendamento.jsx
+++
@@ -1,3 +0,0 @@
-const AtualizarAgendamento = () => <div></div>;
-
-export default AtualizarAgendamento;
--- apps/frontend/src/routes/routes.jsx
+++ apps/frontend/src/routes/routes.jsx
@@ -2,7 +2,6 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import Agendamentos from '../pages/Agendamentos';
 import CriarAgendamento from '../pages/CriarAgendamento';
-import AtualizarAgendamento from '../pages/AtualizarAgendamento';
 
 const AppRoutes = () => (
   <BrowserRouter>
@@ -11,7 +10,6 @@ const AppRoutes = () => (
       <Route path="/agendamentos">
         <Route element={<Agendamentos />} index />
         <Route element={<CriarAgendamento />} path="novo" />
-        <Route element={<AtualizarAgendamento />} path=":id/editar" />
       </Route>
     </Routes>
   </BrowserRouter>
